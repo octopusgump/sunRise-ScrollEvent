9ad8c9cb2ffe3ba146a5d5a4bde349f0fe19aff1
octopusgump <bohaozhang@wustl.edu>
2016-11-02 09:23:49 -0500

    final critique Nov 2nd

---
+++ js/script.js
@@ -0,0 +1,60 @@
+var i = 0;
+$(document).ready(function() {
+
+	$(window).scroll(function (event) {
+		var scroll = $(window).scrollTop();
+		// var pageHeight = $(document).height;
+		var body = document.body,
+		html = document.documentElement;
+
+		var pageHeight = Math.max( body.scrollHeight, body.offsetHeight, 
+			html.clientHeight, html.scrollHeight, html.offsetHeight );
+		
+
+		var windowHeight = $(window).height();
+
+		var height = pageHeight-windowHeight;
+		console.log("scroll: " + scroll);
+		//console.log(height);
+
+		var classSet = [
+		'.a1',//0
+		'.a2',
+		'.a3',
+		'.a4',
+		'.a5',
+		'.a6',
+		];
+		var imageSet = [
+		'cityline.png',//0
+		'train2.gif',
+		'train.gif',
+		'elephant.gif',
+		'lol.png',
+		'lol.png'
+		];
+
+		if (scroll >= height) {
+			$(classSet[i]).append("<img src='gif/"+ imageSet[i] + "'/>");
+			if (classSet[i]=='.a3') {$('.a2').remove();};
+			if (classSet[i]=='.a4') {$('.a1').remove();
+			$('.a4e').append("<img src='gif/elephant.gif'/>");};
+			if (classSet[i]=='.a5') {$('.sun').remove();
+			$('div:first').addClass("sun1");};
+			if (classSet[i]=='.a6') {$('.a3').remove()};	
+			i++;
+				//console.log(imageSet[i]);
+			};
+
+			// if (scroll>= 340 && scroll<1850) {
+			// 	$( this ).on( event );
+			// 	$('.s1').addClass('played');
+			// 	var audio = new Audio('sound/rooster.mp3');
+			// 	audio.play();
+			// };
+
+
+		});
+
+});
+
